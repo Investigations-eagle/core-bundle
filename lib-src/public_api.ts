c6247750f81d87aafa37cc68d9d1960982941c68
Vladymyr Shevchuk <dosankv@gmail.com>
2018-04-22 22:23:39 +0300

    Removed shared components; v1.0.8

--- lib-src/public_api.ts
+++ lib-src/public_api.ts
@@ -1,6 +1,3 @@
-export { TickTockComponent } from './components';
-export { TickTockModule } from './tick-tock.module';
-
 import * as router from '@angular/router';
 import * as core from '@angular/core';
 import * as http from '@angular/http';
@@ -8,5 +5,6 @@ import * as forms from '@angular/forms';
 import * as common from '@angular/common';
 import * as platformBrowser from '@angular/platform-browser';
 import * as platformBrowserDynamic from '@angular/platform-browser-dynamic';
+
 export const ng = {router, http, forms, common, core, platformBrowser, platformBrowserDynamic};
 
