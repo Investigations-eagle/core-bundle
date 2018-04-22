a5742cdccee59555d921c1415606087224241d7b
Serhii Krechko <serhii.krechko@eleks.com>
2018-04-22 17:09:09 +0300

    Local Cleanup

---
+++ src/core.ts
@@ -0,0 +1,17 @@
+import * as Router from '@angular/router';
+import * as Core from '@angular/core';
+import * as Http from '@angular/http';
+import * as Forms from '@angular/forms';
+import * as Common from '@angular/common';
+import * as PlatformBrowser from '@angular/platform-browser';
+import * as PlatformBrowserDynamic from '@angular/platform-browser-dynamic';
+
+export {
+  Router,
+  Core,
+  Forms,
+  Http,
+  Common,
+  PlatformBrowser,
+  PlatformBrowserDynamic
+};
